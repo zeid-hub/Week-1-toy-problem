a56d6376c203b48a53e3c2a676ec8e337d4e656e
zeid <zeidhassan4@gmail.com>
2023-12-17 10:54:11 +0300

    Complited assignment

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Week-1-toy-problem
+# GRADE.JS
+Score    Grades
+>79        A
+60-79      B
+49-59      C
+40-49      D
+0-40       E
+
+
---
+++ grade.js
@@ -0,0 +1,18 @@
+let score = 65
+let grades;
+if (score > 79){
+   return A
+}
+else if (score >= 60){
+  return B
+}
+else if (score >=50){
+  return C
+}
+else if (score >=40){
+  return D
+}
+else{
+    return E
+}
+console.log(grades);
